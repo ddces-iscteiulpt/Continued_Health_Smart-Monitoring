f6baac52ae54fb39bb2cbf3c415b3533ed57e1c6
dc <daniel_cale@iscte-iul.pt>
2021-06-24 14:40:01 +0100

    pacientes

--- Sensors_Arduino/sketch_jun22a/sketch_jun22a.ino
+++ Sensors_Arduino/sketch_jun22a/sketch_jun22a.ino
@@ -1,9 +1,54 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Wire.h> // I2C library, required for MLX90614
+#include <SparkFunMLX90614.h> //Click here to get the library: http://librarymanager/All#Qwiic_IR_Thermometer by SparkFu
 
+IRTherm therm; // Create an IRTherm object to interact with throughout
+
+const int GSR=A1;
+int sensorValue=0;
+int gsr_average=0;
+ 
+void setup(){
+  Serial.begin(115200); // Initialize Serial to log output
+  Wire.begin(); //Joing I2C bus
+  
+  if (therm.begin() == false){ // Initialize thermal IR sensor
+    Serial.println("Qwiic IR thermometer did not acknowledge! Freezing!");
+    while(1);
+     }
+  Serial.println("Qwiic IR Thermometer did acknowledge.");
+  
+  therm.setUnit(TEMP_C); // Set the library's units to Farenheit
+  // Alternatively, TEMP_F can be replaced with TEMP_C for Celsius or
+  // TEMP_K for Kelvin.
+  
+  pinMode(LED_BUILTIN, OUTPUT); // LED pin as output
 }
+ 
+void loop(){
+  long sum=0;
+  for(int i=0;i<10;i++)           //Average the 10 measurements to remove the glitch
+      {
+      sensorValue=analogRead(GSR);
+      sum += sensorValue;
+      delay(5);
+      }
+   gsr_average = sum/10;
+   Serial.println(gsr_average);
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+   digitalWrite(LED_BUILTIN, HIGH);
+    
+  // Call therm.read() to read object and ambient temperatures from the sensor.
+  if (therm.read()) // On success, read() will return 1, on fail 0.
+  {
+    // Use the object() and ambient() functions to grab the object and ambient
+ // temperatures.
+  // They'll be floats, calculated out to the unit you set with setUnit().
+    Serial.print("Object: " + String(therm.object(), 2));
+    Serial.println("C");
+    Serial.print("Ambient: " + String(therm.ambient(), 2));
+    Serial.println("C");
+    Serial.println();
+  }
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
 }
